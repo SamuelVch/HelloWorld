0124698a57e9ddc13d9c3504b629cfb6af0ade68
SamuelVch <71339522+SamuelVch@users.noreply.github.com>
2020-12-30 15:44:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo "Il est l'heure de manger";
 echo "Ce soir, c'est soupe aux champignons";
 echo "Coucou";
 echo "Salut à vous";
-echo "J'ai modifié le fichier"
+echo "J'ai modifié le fichier";
    echo "Le site des bonnes nouvelles à changé ma vie, depuis le soleil est rayonnant";
 
    echo "<h1> VIVE LES CACAHUÈTES </h1>";
